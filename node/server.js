0d3de061003646e201baae93ce41dd124bb09f6d
Italo Alcântara <italoalcantara6@gmail.com>
2026-03-06 18:00:17 -0300

    Updated server.js

--- node/server.js
+++ node/server.js
@@ -2,6 +2,20 @@ import { fastify } from 'fastify'
 
 const server = fastify()
 
+server.post('/videos', () => {
+  return "POST"
+})
+
+server.get('/videos', () => {
+  return "GET"
+})
+
+server.post('/videos', () => {
+  return "POST"
+})
+server.post('/videos', () => {
+  return "POST"
+})
 
 server.listen({
   port: 3000
