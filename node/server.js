32dfa528eb7ad8e8fede14f5cecdee2b318b2e77
Italo Alcântara <italoalcantara6@gmail.com>
2026-03-06 18:25:09 -0300

    Updated server.js

--- node/server.js
+++ node/server.js
@@ -3,10 +3,10 @@ import { DBMemory } from './database-in-memory.js'
 import { useParams } from "react-router-dom"
 
 const server = fastify()
-const DBMemory = new DBMemory()
+const dataBaseMemory = new DBMemory()
 
 server.post('/videos', (video) => {
-  DBMemory.create(video)
+  dataBaseMemory.create(video)
   
   return {
     message: "Vídeo adicionado!"
@@ -14,19 +14,19 @@ server.post('/videos', (video) => {
 })
 
 server.get('/videos', () => {
-  return DBMemory.listAll()
+  return dataBaseMemory.listAll()
 })
 
 server.put('/videos/:id', (video) => {
   const { id } = useParams()
 
-  DBMemory.put(id, video)
+  dataBaseMemory.put(id, video)
 })
 
 server.delete('/videos/:id', () => {
   const { id } = useParams()
 
-  DBMemory.delete(id)
+  dataBaseMemory.delete(id)
 })
 
 server.listen({
