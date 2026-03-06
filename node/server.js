2bacb6ad96565ea11668d07027d68209ab6543da
Italo Alcântara <italoalcantara6@gmail.com>
2026-03-06 17:32:44 -0300

    Updated server.js

--- node/server.js
+++ node/server.js
@@ -1,12 +1,9 @@
 import { createServer } from 'node:http'
 
-const server = createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('Servidor rodando!')
-})
-
 const PORT = 3000
 
-server.listen(PORT, () => {
+const server = createServer(() => {
   console.log(`API rodando na porta ${PORT}`)
 })
+
+server.listen(3000)
